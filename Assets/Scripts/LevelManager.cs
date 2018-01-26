23689a9250fe10c95efdaf89ea4d0fe484da90ae
Unknown <peter@petermehlmann.com>
2018-01-26 20:24:03 +0100

    Menu system
    Menu and basic scenes to start off the game dev

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour
+{
+
+    public void LoadLevel(string name)
+    {
+        Debug.Log("New Level load: " + name);
+      //  Application.LoadLevel(name);
+		SceneManager.LoadScene(name);
+    }
+
+    public void QuitRequest()
+    {
+        Debug.Log("I want to quit");
+        Application.Quit();
+    }
+
+}
