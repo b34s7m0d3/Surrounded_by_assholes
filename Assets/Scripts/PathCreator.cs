9e05d29c639e87cde4a71e3e5bdb3d87250b84b5
Tim Hoffmann <tim@hoffmannprojects.com>
2018-01-27 16:26:43 +0100

    1st iteration of solution for instantiating waypoints at the right place.

--- Assets/Scripts/PathCreator.cs
+++ Assets/Scripts/PathCreator.cs
@@ -3,18 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class PathCreator : MonoBehaviour {
+public class PathCreator : MonoBehaviour
+{
 
     public GameObject prefab;
     public GameObject parent;
 
     void OnMouseDown()
     {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray))
-        {
-            GameObject childObject = Instantiate(prefab, transform.position, transform.rotation) as GameObject;
-            childObject.transform.parent = parent.transform;
-        }
+        // Calculate rounded position from clicked position.
+        Vector2 rawPosistion = CalculateWorldPointOfClick();
+        Vector2 snappedPosition = SnapToWorldGrid(rawPosistion);
+
+        SpawnWaypoint(prefab, snappedPosition);
     }
+
+    Vector2 CalculateWorldPointOfClick()
+    {
+        // Get camera component of first found camera object.
+        Camera camera = FindObjectOfType<Camera>().GetComponent<Camera>();
+
+        // Calculate world units from mouse position (pixels).
+        return camera.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    Vector2 SnapToWorldGrid(Vector2 rawWorldPosition)
+    {
+        int roundedX, roundedY;
+        roundedX = Mathf.RoundToInt(rawWorldPosition.x);
+        roundedY = Mathf.RoundToInt(rawWorldPosition.y);
+        return new Vector2(roundedX, roundedY);
+    }
+
+    void SpawnWaypoint(GameObject obj, Vector2 position)
+    {
+        GameObject newWaypoint = Instantiate(obj, position, Quaternion.identity) as GameObject;
+        newWaypoint.transform.parent = parent.transform;
+    }
+
 }
