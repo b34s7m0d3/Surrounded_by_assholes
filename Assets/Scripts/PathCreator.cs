7e01948211ac2170cc074cd9cfa12b1c0a095936
Unknown <peter@petermehlmann.com>
2018-01-27 15:20:27 +0100

    Can instantiate cubes that will be followed
    missing: instantiate at right place

---
+++ Assets/Scripts/PathCreator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PathCreator : MonoBehaviour {
+
+    public GameObject prefab;
+    public GameObject parent;
+
+    void OnMouseDown()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray))
+        {
+            GameObject childObject = Instantiate(prefab, transform.position, transform.rotation) as GameObject;
+            childObject.transform.parent = parent.transform;
+        }
+    }
+}
