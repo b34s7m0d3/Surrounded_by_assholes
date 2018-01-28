1f13f60dbd55f0fa4227ff5c566ac31ee6a4bb8e
Unknown <peter@petermehlmann.com>
2018-01-28 12:45:33 +0100

    PlayerKiller
    there is now a new enemyType

---
+++ Assets/Scripts/PlayerKiller.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerKiller : MonoBehaviour {
+
+    GameObject StuffToKill;
+    public float speed = 4.5f;
+    public bool move = false;
+
+	// Use this for initialization
+	void Start () {
+
+        StuffToKill = GameObject.Find("player");
+
+	}
+	
+   
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        move = true;
+        Debug.Log("maaan");
+    }
+
+
+
+
+    private void Update()
+    {
+        Vector2 dir = StuffToKill.transform.position - this.transform.localPosition;
+
+        float distThisFrame = speed * Time.deltaTime;
+        if (move)
+        {
+            
+                // Move towards node
+                transform.Translate(dir.normalized * distThisFrame, Space.World);
+        }
+        
+    }
+    
+
+
+}
